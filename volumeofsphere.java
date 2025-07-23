f92053f49e98a6cec31f88ade5941f82ce2816e2
amith-0609 <yakkalaamith@gmail.com>
2025-07-23 11:22:21 +0530

    Add files via upload

---
+++ volumeofsphere.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+class volumeofsphere {
+ public static void main(String[] args) {
+	 int r;
+	 float pi=3.14f;
+	 double vol;
+	 System.out.print("Enter the radius");
+     Scanner sc = new Scanner(System.in);
+     r = sc.nextInt();  
+     vol = 4/3*pi*r*r*r;
+     System.out.print("Volume of Sphere is : " +vol);
+     
+     
+     
+     
+ }
+}
+
+
