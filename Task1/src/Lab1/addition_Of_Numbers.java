31e9c3ae16c36bbf0a5e9933ae1e21de82ddaaf2
amith-0609 <yakkalaamith@gmail.com>
2025-08-04 08:24:22 +0530

    Add files via upload

---
+++ Task1/src/Lab1/addition_Of_Numbers.java
@@ -0,0 +1,24 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class addition_Of_Numbers {
+
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the Morning Shift Earnings : ");
+		int morningShiftEarnings = sc.nextInt();
+		
+		System.out.print("Enter the Evening Shift Earnings : ");
+		int eveningShiftEarnings = sc.nextInt();
+		
+		System.out.println("The total amount earned in a day : "+(morningShiftEarnings+eveningShiftEarnings));
+		
+		
+		
+		
+		
+	}
+}
